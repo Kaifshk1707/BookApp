e6d6f6fc08a703d3e31b110f7f21d70fab6cc125
haroon <skharoon21@gmail.com>
2025-03-18 16:21:47 +0530

    some files change.

--- src/TabScreens/Search.js
+++ src/TabScreens/Search.js
@@ -1,82 +1,10 @@
-import { View, FlatList, ActivityIndicator, Modal } from "react-native";
-import React, { useEffect, useState } from "react";
-import {
-  getBookData,
-  getElectronicData,
-  handleDeletePost,
-  handleDeletePostSearch,
-} from "../API/book";
-import HomeComponent from "../components/HomeComponent";
-import AddButton from "../components/AddButton";
-import AddBookScreen from "../screeens/AddBookScreen";
+import { View, Text } from "react-native";
+import React from "react";
 
 const Search = () => {
-  const [electronicList, setElectronicList] = useState([]);
-  const [modalVisible, setModalVisible] = useState(false);
-  const [selectedItem, setSelectedItem] = useState({});
-
-  const getListOfBook = () => {
-    getElectronicData({
-      onSuccess: (data) => setElectronicList(data),
-      onError: (err) => console.log(err),
-    });
-  };
-
-  useEffect(() => {
-    getListOfBook();
-  }, []);
-
-  const handleDeleteItem = (item) => {
-    console.log(item.id);
-    handleDeletePostSearch({
-      onSuccess: () => getListOfBook(),
-      onError: (err) => console.log(err),
-      itemID: item.id,
-    });
-  };
-
-  const handleEditPost = (item) => {
-    setModalVisible(true);
-    setSelectedItem(item);
-  };
-
   return (
-    <View style={{ flex: 1, padding: 15, backgroundColor: "#F5F5F5" }}>
-      {electronicList.length > 0 ? (
-        <FlatList
-          // horizontal
-          data={electronicList}
-          keyExtractor={(item) => item.id.toString()}
-          contentContainerStyle={{ paddingBottom: 20 }}
-          renderItem={({ item }) => (
-            <HomeComponent
-              title={item.name_of_item}
-              BookTitle={item.details_item}
-              price={item.price_of_item}
-              email={item.name_of_shop}
-              imageURL={item.image_item}
-              date={item.createdAt}
-              onDeleteItem={() => handleDeleteItem(item)}
-              onEditItem={() => handleEditPost(item)}
-            />
-          )}
-        />
-      ) : (
-        <ActivityIndicator size={"large"} color={"#A7CCF6"} />
-      )}
-      <AddButton
-        onPress={() => {
-          setModalVisible(true);
-          setSelectedItem({});
-        }}
-      />
-      <Modal visible={modalVisible} animationType="slide">
-        <AddBookScreen
-          onCloseModal={() => setModalVisible(false)}
-          createNewSuccess={() => getListOfBook()}
-          selectedItem={selectedItem}
-        />
-      </Modal>
+    <View>
+      <Text>Search</Text>
     </View>
   );
 };
